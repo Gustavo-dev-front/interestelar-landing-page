64fc91521966e17d1b615e7c69a4756209f040f9
Gustavo Dias <gustavoalvesdiaz08@gmail.com>
2024-01-19 21:41:04 -0300

    projeto finalizado

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,8 @@
-console.log("teste");
+const HEADER = document.querySelector(".header");
+let scrolled = false;
 
+window.addEventListener("scroll", () => {
+  if (!scrolled & (window.scrollY >= 142))
+    HEADER.classList.add("header--scrolled");
+  else HEADER.classList.remove("header--scrolled");
+});
